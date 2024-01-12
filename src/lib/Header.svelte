25d2b4d02c07135c88fc435717646ccd69aaef31
DanielKerrigan <d.kerrigan111@gmail.com>
2024-01-12 10:18:40 -0500

    resume

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -5,6 +5,6 @@
 		<li><a href="/research">Research</a></li>
 		<li><a href="/teaching">Teaching</a></li>
 		<li><a href="/other">Other</a></li>
-		<li><a rel="external" href="/daniel-kerrigan-resume.pdf">Resume</a></li>
+		<li><a rel="external" href="/daniel_kerrigan_resume.pdf">Resume</a></li>
 	</ul>
 </nav>
